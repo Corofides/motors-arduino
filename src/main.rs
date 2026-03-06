e041aca4948ba4826a15c789be2b7f420761da42
corofides <jamlendrem@gmail.com>
2026-03-06 20:38:58 +0000

    Fix circuit; Allow ability to go backwards; There seems to be some jank with going backwards, not sure why;

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,21 @@ struct PWMControl {
 }
 
 impl PWMControl {
+    fn clear(&self) {
+    }
     fn pulse(&self) {
-        self.pin_control.toggle_pin();
+        match self.direction {
+            Direction::Forward => {
+                self.pin_control.toggle_pin(&self.forward_pin);
+            },
+            Direction::Backward => {
+                self.pin_control.toggle_pin(&self.backward_pin);
+            }
+        }
     }
 }
 
+#[derive(PartialEq)]
 enum Output {
     P_12,
     P_13,
@@ -39,8 +49,17 @@ struct PinControl {
 }
 
 impl PinControl {
-    pub fn toggle_pin(&self) {
-        self.port.pinb.write(|w| w.pb5().set_bit());
+    pub fn toggle_pin(&self, output: &Output) {
+        match output {
+            Output::P_12 => {
+                self.port.pinb.write(|w| w.pb5().clear_bit());
+                self.port.pinb.write(|w| w.pb4().set_bit());
+            },
+            Output::P_13 => {
+                self.port.portb.write(|w| w.pb4().clear_bit());
+                self.port.pinb.write(|w| w.pb5().set_bit());
+            }
+        }
     }
 }
 
@@ -79,6 +98,16 @@ fn main() -> ! {
         w.pb4().set_bit(); // Pin 12;
         w.pb5().set_bit()  // Pin 13;
     });
+
+    /*dp.PORTB.ddrb.write(|w| {
+        w.pb1().set_bit();
+        w.pb2().set_bit();
+        w.pb3().set_bit();
+        w.pb4().set_bit(); // Pin 12;
+        w.pb5().set_bit()  // Pin 13;
+    });*/
+
+    //dp.PORTB.portb.write(|w| w.pb4().set_bit());
     
     avr_device::interrupt::free(|cs| {
         let pin_control = PinControl {
@@ -86,7 +115,7 @@ fn main() -> ! {
         };
 
         let pwm_control = PWMControl {
-            direction: Direction::Forward,
+            direction: Direction::Backward,
             forward_pin: Output::P_13,
             backward_pin: Output::P_12,
             pin_control: pin_control,
